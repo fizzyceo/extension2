24807b2b42c9ea39145c87922d8969154de5cd8d
fizzyceo <84289623+fizzyceo@users.noreply.github.com>
2023-03-11 10:50:55 -1100

    Update assistantExtension.js

--- assistantExtension.js
+++ assistantExtension.js
@@ -1,27 +1,28 @@
 
 
-let textPlace = document.getElementsByTagName("p")[0]
-let response;
-console.log("this is the assistante");
-//this is not working
-const fetchingResults  = async (email,title)=>{
-response = await fetch("https://kyo-mail.onrender.com/mail",{
-method:'POST',    
-body:JSON.stringify({
-    email:message.emailResponse,
-    subject:message.title
-})
-})
-}
-chrome.runtime.onMessage.addListener(
-    
-    function(message, sender, sendResponse) {
-        console.log(" im in ");
-            //  To do something
-            console.log(message.title);
-            if(message.to ==='popup'){
-            console.log(message.emailResponse)
-            fetchingResults(message.emailResponse,message.title)
+    let textPlace = document.getElementsByTagName("p")[0]
+    let response;
+    console.log("this is the assistante");
+    //this is not working
+    const fetchingResults  = async (email,title)=>{
+    response = await fetch("https://kyo-mail.onrender.com/mail",{
+    method:'POST',    
+    body:JSON.stringify({
+        email:email,
+        subject:title
+    })
+    })
+    console.log(response);
+    }
+    chrome.runtime.onMessage.addListener(
+        //the popup needs to be open for the script to run correctly 
+        function(message, sender, sendResponse) {
+            console.log(" im in ");
+                //  To do something
+                console.log(message.title);
+                if(message.to ==='popup'){
+                console.log(message.emailResponse)
+                fetchingResults(message.emailResponse,message.title)
             textPlace.textContent = response.reply            
             }
     }
